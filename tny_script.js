e6970eed3d97f005e1886c0d177213b1fbfdfae5
Connor Webdale <CWebdale006@west-mec.org>
2019-02-05 15:16:29 -0700

    Set up files
    Changes file names, linked JavaScript file, added name into comments.

--- tny_script.js
+++ tny_script.js
@@ -1,11 +1,14 @@
+"use strict";
+
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Tutorial Case
 
    Countdown Clock
-   Author: 
-   Date:   
+   Author: Connor J Webdale 
+   Date: 2.5.19 
+
+   Filename: tny_script.js 
 
 */
-
